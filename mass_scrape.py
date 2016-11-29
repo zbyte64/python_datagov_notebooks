4ffc0c83ded38a6ddbdeb7f5f3a184cf13e7dd20
Jason Kraus <zbyte64@gmail.com>
2016-11-28 19:33:12 -0800

    ignore derrived datasets, complete flags, write metadata, more async

--- mass_scrape.py
+++ mass_scrape.py
@@ -1,4 +1,5 @@
 import aiohttp, asyncio, pandas, os, requests, collections, urllib, shutil
+from concurrent.futures import TimeoutError
 from time import sleep
 import json
 
@@ -9,15 +10,26 @@ infinity = float('inf')
 async def peek_if_csv_and_write(dataset, max_readline=1024*12, max_size=infinity, allow_no_size=True, parent_folder='datasets'):
     if not os.path.exists(parent_folder):
         os.mkdir(parent_folder)
-    url = dataset['res_url'][dataset['res_format'].index('CSV')]
+
     folder = os.path.join(parent_folder, dataset['id'])
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+
     complete_flag = os.path.join(folder, 'complete')
     if os.path.exists(complete_flag):
         #print(folder, "Already exists")
         return True
-    elif not os.path.exists(folder):
-        os.mkdir(folder)
+
     json.dump(dataset, open(os.path.join(folder, '_metadata.json'), 'w'), indent='\t')
+
+    url = dataset['res_url'][dataset['res_format'].index('CSV')]
+    for index, res_description in enumerate(dataset['res_description']):
+        if '.original.' in res_description:
+            print(folder, 'skipping derrived dataset')
+            return False
+            url = dataset['res_url'][index]
+
+
     host = urllib.parse.urlparse(url).netloc
     await host_semaphores[host].acquire()
     print(folder, "Downloading:", url)
@@ -32,6 +44,8 @@ async def peek_if_csv_and_write(dataset, max_readline=1024*12, max_size=infinity
                 #content-length
                 if 'CONTENT-DISPOSITION' not in resp.headers:
                     print(folder, "no content disposition found")
+                    print(folder, "writing complete flag")
+                    open(complete_flag, 'w').close()
                     return False
                 dst = resp.headers['CONTENT-DISPOSITION'].split('; filename=')[-1]
                 dst = os.path.join(folder, dst)
@@ -83,43 +97,64 @@ async def peek_if_csv_and_write(dataset, max_readline=1024*12, max_size=infinity
                     #TODO unzip
                 else:
                     print(folder, "Unrecognized filetype:", dst)
-                    return False
+                    bytes_so_far = 0
+                    output = open(dst, 'wb')
+                    while True:
+                        if bytes_so_far > max_size:
+                            return False #TOO big of a file and they didn't tell us how big!
+                        chunk = await resp.content.read()
+                        if chunk:
+                            bytes_so_far += len(chunk)
+                            output.write(chunk)
+                        else:
+                            break
+                    output.close()
 
                 print(folder, "writing complete flag")
                 open(complete_flag, 'w').close()
                 return dst
-        except asyncio.TimeoutError:
+        except (asyncio.TimeoutError, TimeoutError):
             print(folder, "timed out")
-            shutil.rmtree(folder, True)
             return False
         except (aiohttp.errors.ServerDisconnectedError, ConnectionResetError) as error:
             print(folder, "angry server", error)
-            shutil.rmtree(folder, True)
             return False
         finally:
             host_semaphores[host].release()
 
 
+async def que_datasets(url, params):
+    async with aiohttp.ClientSession() as session:
+        resp_with_timer = await session.get(url, params=params) #asyncio.wait_for(session.get(url, params=params), 5.0)
+        async with resp_with_timer as resp:
+            if resp.status != 200:
+                print("got non 200 response:", resp.status)
+                return [], 0
+            json_response = await resp.json()
+            results = json_response['results']
+            futures = map(peek_if_csv_and_write, results)
+            return futures, json_response['count']
+
+
+async def download_data_gov():
+    i = 0
+    while True:
+        print('Downloading page:', i)
+        offset = i * 10
+        futures, count = await que_datasets(
+            'http://catalog.data.gov/api/search/dataset',
+            {'all_fields':1, 'res_format':'CSV', 'offset': offset}
+        )
+
+        await asyncio.gather(*futures, asyncio.sleep(5))
+
+        i += 1
+
+        if count < offset + 10:
+            break
+
+
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
-
-#download all the csvs
-#TODO turn into coroutine
-i = 0
-while True:
-    offset = i * 10
-    raw_response = requests.get('http://catalog.data.gov/api/search/dataset', params={'all_fields':1, 'res_format':'CSV', 'offset': offset})
-    try:
-        response = raw_response.json()
-    except:
-        print("ERROR", ras_response.status)
-        sleep(5)
-        continue
-    results = response['results']
-    futures = map(peek_if_csv_and_write, results)
-    loop.run_until_complete(asyncio.gather(*futures))
-
-    if response['count'] < offset + 10:
-        break
-
+loop.run_until_complete(download_data_gov())
 loop.close()
